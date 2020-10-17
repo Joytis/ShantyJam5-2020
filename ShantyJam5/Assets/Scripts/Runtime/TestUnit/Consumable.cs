a77f822dbba517a5da41c75b06802ff123a3f124
jayde225 <jayde225@users.noreply.github.com>
2020-10-17 16:24:45 -0500

    asdg

--- ShantyJam5/Assets/Scripts/Runtime/TestUnit/Consumable.cs
+++ ShantyJam5/Assets/Scripts/Runtime/TestUnit/Consumable.cs
@@ -14,6 +14,7 @@ public class Consumable : MonoBehaviour
         if (coll.gameObject.tag == "Player")
         {
             Debug.Log("Consumable Consumed :)");
+            Debug.Log("Girth Value is now: " + GrithValue);
 
             ThingConsumed?.Invoke(this);
             Destroy(gameObject);
