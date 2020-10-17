8e63be4fea9528bb0f0e0e2d8cf55df9f98a5746
Ran Crump <rancrump18@gmail.com>
2020-10-16 22:23:38 -0500

    Added GameState/GameManager

---
+++ ShantyJam5/Assets/Scripts/GameState/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    
+    // Event delegates
+    public delegate void GameOverEvent ();
+    
+    public delegate void GameStartEvent ();    
+
+    // Static Variables
+    public static GameManager instance;
+    public static GameOverEvent gameOverEvent;
+    public static GameStartEvent gameStartEvent;
+    private void Awake()
+    {
+        /* Sry Clark I had to do it to em        
+                         ⣠⣦⣤⣀
+                    ⠀⠀⠀⠀⢡⣤⣿⣿
+                    ⠀⠀⠀⠀⠠⠜⢾⡟
+                    ⠀⠀⠀⠀⠀⠹⠿⠃⠄
+                    ⠀⠀⠈⠀⠉⠉⠑⠀⠀⠠⢈⣆
+                    ⠀⠀⣄⠀⠀⠀⠀⠀⢶⣷⠃⢵
+                    ⠐⠰⣷⠀⠀⠀⠀⢀⢟⣽⣆⠀⢃
+                    ⠰⣾⣶⣤⡼⢳⣦⣤⣴⣾⣿⣿⠞
+                    ⠀⠈⠉⠉⠛⠛⠉⠉⠉⠙⠁
+                    ⠀⠀⡐⠘⣿⣿⣯⠿⠛⣿⡄
+                    ⠀⠀⠁⢀⣄⣄⣠⡥⠔⣻⡇
+                    ⠀⠀⠀⠘⣛⣿⣟⣖⢭⣿⡇
+                    ⠀⠀⢀⣿⣿⣿⣿⣷⣿⣽⡇
+                    ⠀⠀⢸⣿⣿⣿⡇⣿⣿⣿⣇
+                    ⠀⠀⠀⢹⣿⣿⡀⠸⣿⣿⡏
+                    ⠀⠀⠀⢸⣿⣿⠇⠀⣿⣿⣿
+                    ⠀⠀⠀⠈⣿⣿⠀⠀⢸⣿⡿
+                    ⠀⠀⠀⠀⣿⣿⠀⠀⢀⣿⡇
+                    ⠀⣠⣴⣿⡿⠟⠀⠀⢸⣿⣷
+                    ⠀⠉⠉⠁⠀⠀⠀⠀⢸⣿⣿⠁
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈
+        */
+        if (instance == null)  
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(instance);
+            instance = this;
+        }        
+    }
+    
+    // Event Envokers
+    public void GameOver()
+    {
+        gameOverEvent ();
+    }
+    public void GameStart()
+    {
+        gameStartEvent ();
+    }
+}
