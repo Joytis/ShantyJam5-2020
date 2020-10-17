61d92d2061d24cffe3f512c0bb631d96310a3d99
Clark Chambers <clarkchambers94@gmail.com>
2020-10-16 22:30:05 -0500

    Working on stuff!
    njkldwnajkldwa

--- ShantyJam5/Assets/Scripts/BirdMover.cs
+++ ShantyJam5/Assets/Scripts/BirdMover.cs
@@ -7,12 +7,6 @@ public class BirdMover : MonoBehaviour
     [SerializeField] Rigidbody2D _rigidbody = null;
     [SerializeField] float _birdLateralSpeed = 20.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
