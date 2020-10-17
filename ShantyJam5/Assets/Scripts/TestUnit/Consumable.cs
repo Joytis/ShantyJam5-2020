10ad8040fd5ffdacf0e695a03afbf38bf1c315b9
Ran Crump <rancrump18@gmail.com>
2020-10-17 02:57:12 -0500

    Yee Haw (Scene Changes pls refresh scene)

---
+++ ShantyJam5/Assets/Scripts/TestUnit/Consumable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : MonoBehaviour
+{
+    public float girthValue = 5;
+    private void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "Player")
+        {
+            Debug.Log("Consumable Consumed :)");
+            Girthodometer.instance.AddGirth(girthValue);
+            Destroy(gameObject);
+        }
+    }
+}
