d16c9552af317a6ef29ab72821d604f34ccad28a
Clark Chambers <clarkchambers94@gmail.com>
2020-10-16 20:55:11 -0500

    Wow! A save!
    Check this out!

---
+++ ShantyJam5/Assets/Scripts/BirdMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D _rigidbody = null;
+    [SerializeField] float _birdLateralSpeed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveRight()
+    {
+        _rigidbody.AddForce(new Vector2(_birdLateralSpeed, _birdLateralSpeed) * Time.deltaTime);
+    }
+}
