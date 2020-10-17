b764a8ab71e78e46d9f97c7e6d879a77a0efc452
Clark Chambers <clarkchambers94@gmail.com>
2020-10-17 08:55:23 -0500

    Added assets from Dusky/Tile Brushes!

--- ShantyJam5/Assets/Scripts/Runtime/GameState/GameManager.cs
+++ ShantyJam5/Assets/Scripts/Runtime/GameState/GameManager.cs
@@ -1,20 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     
-    // Event delegates
-    public delegate void GameOverEvent ();
-    
-    public delegate void GameStartEvent ();    
 
     // Static Variables
     public static GameManager instance;
-    public static GameOverEvent gameOverEvent;
-    public static GameStartEvent gameStartEvent;
+    // // Event delegates
+    // public delegate void GameOverEvent ();
+    // public delegate void GameStartEvent ();    
+    // public static GameOverEvent gameOverEvent;
+    // public static GameStartEvent gameStartEvent;
+    // NOTE(clark): Hey Ran! I noticed you were using some olden-days C# syntax for events. Try this out!:
+    //              It's the same thing (Action is just a delegate that takes no arguments and returns no values. 
+    //              Action<T> will accept generic arguments), but you have a more constrained API (you can only
+    //              subscribe and ubsubscribe publically and invoke privately). I'd usually slap this in code review
+    //              or something, but I'm probably just going to forget otherwise lol. 
+    public event Action gameOverEvent;
+    public event Action gameStartEvent;
+
     private void Awake()
     {
+        // NOTE(clark): :'c
         /* Sry Clark I had to do it to em        
                          ⣠⣦⣤⣀
                     ⠀⠀⠀⠀⢡⣤⣿⣿
@@ -50,10 +57,12 @@ public class GameManager : MonoBehaviour
     // Event Envokers
     public void GameOver()
     {
-        gameOverEvent ();
+        // NOTE(clark): null game events will throw excepetions! Null-coalescing will check for that before calling. 
+        gameOverEvent?.Invoke();
     }
     public void GameStart()
     {
-        gameStartEvent ();
+        // NOTE(clark): null game events will throw excepetions! Null-coalescing will check for that before calling. 
+        gameStartEvent?.Invoke();
     }
 }
