b9dc48527b3f2f4d1bc215cdcd4b5edff3db854b
Clark Chambers <clarkchambers94@gmail.com>
2020-10-17 09:41:43 -0500

    Changed up some sprites. added code for Jayde!

---
+++ ShantyJam5/Assets/Scripts/Runtime/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] Transform[] _locations = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomIndex = Random.Range(0, _locations.Length);
+        Transform randomTransform = _locations[randomIndex];
+        Debug.Log(randomTransform.name);
+        
+    }
+}
