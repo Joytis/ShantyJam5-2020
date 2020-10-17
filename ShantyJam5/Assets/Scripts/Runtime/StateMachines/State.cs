d523194e2b6d94bbe6f14fe8c3454be9dbc692a4
Clark Chambers <clarkchambers94@gmail.com>
2020-10-17 16:02:00 -0500

    Bunch of asset changes and some animations.

---
+++ ShantyJam5/Assets/Scripts/Runtime/StateMachines/State.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+// Should be used as a state in the Finite state machine.
+// Enter: Called when the system first enters the listed state
+// Exit: Called when the system leaves the state
+// Update: Called every frame by the state system update().  
+public class State : IState 
+{
+	public Action enter { get; set; } = () => {};
+	public Action exit { get; set; } = () => {};
+	public Action update { get; set; } = () => {};
+
+	public virtual void Enter() => enter();
+	public virtual void Exit() => exit();
+	public virtual void Update() => update();
+}
