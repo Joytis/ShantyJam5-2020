19b9369d2684034a2cf2dde02cd5e71ecc524d15
Clark Chambers <clarkchambers94@gmail.com>
2020-10-17 12:26:13 -0500

    Working on animation right now!
    HECK YEAH

---
+++ ShantyJam5/Assets/Scripts/Runtime/BirdDisplay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BirdDisplay : MonoBehaviour
+{
+
+    [SerializeField] 
+    private Animator _animator;
+
+    public void ShowFlipped(Vector2 velocity)
+    {
+        // Don't do anything if we're just standing still. 
+        if(Mathf.Approximately(velocity.x, 0f)) return;
+
+        // Check if we're moving left or right!
+        bool leftOrRight = velocity.x < 0f;
+        transform.localScale = new Vector3(leftOrRight ? 1f : -1f, 1f, 1f);
+    }
+
+    public void ShowIdle()
+    {
+        _animator.Play("Idle");
+    }
+
+    public void ShowWalking()
+    {
+        _animator.Play("Walking");
+    }
+
+    public void ShowJumping()
+    {
+        _animator.Play("Jumping");
+    }
+
+}
