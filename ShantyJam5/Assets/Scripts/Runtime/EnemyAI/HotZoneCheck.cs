7078f9b688998098da6384239040c7abed366694
harleigh93 <harleigh.m.bass@gmail.com>
2020-10-17 18:03:59 -0500

    EnemyBird AI!
    ai for evil birdie

---
+++ ShantyJam5/Assets/Scripts/Runtime/EnemyAI/HotZoneCheck.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotZoneCheck : MonoBehaviour
+{
+    private EnemyBehavior enemyParent;
+    private bool inRange;
+    private Animator anim;
+
+    private void Awake()
+    {
+        enemyParent = GetComponentInParent<EnemyBehavior>();
+        anim = GetComponentInParent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack.anim"))
+        {
+            enemyParent.Flip(); // keep looking in player direction
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+
+    // Player exits hotZone
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+            gameObject.SetActive(false);
+            enemyParent.triggerArea.SetActive(true);
+            enemyParent.inRange = false; // so enemy can go back to patrol state
+            enemyParent.SelectTarget(); // figures out new target
+        }
+    }
+}
