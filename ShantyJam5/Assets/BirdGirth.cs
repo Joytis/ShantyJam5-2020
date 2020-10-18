f7ed91f90a6c2268d070159070effa1da021246e
Ran Crump <rancrump18@gmail.com>
2020-10-18 12:00:42 -0500

    + Bird Changes size on eating a worm \n+ Test Worm spawns the same size as the prefab

--- ShantyJam5/Assets/BirdGirth.cs
+++ ShantyJam5/Assets/BirdGirth.cs
@@ -4,14 +4,21 @@ public class BirdGirth : MonoBehaviour
 {
     
     public int currentHealth = 25;
-    public bool gameState_Lose = false;
-    public bool gameState_win = false;
-    public HealthBar healthBar;
+    public bool gameState_Lose = false, gameState_win = false;    
+    public HealthBar healthBar;    
+    public float girthAddSize = 2, scale;
 
     // Start is called before the first frame update
     void Start()
     {
         healthBar.SetHealth(currentHealth);
+        Consumable.ThingConsumed += ConsumableConsumed;
+    }
+
+    void ConsumableConsumed(Consumable c) 
+    {
+        this.scale *= girthAddSize;
+        this.transform.localScale = new Vector3(this.scale, this.scale, this.scale);
     }
 
     void OnEnable() 
