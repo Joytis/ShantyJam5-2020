921ad63cef6c55343de58fa6d973b2da02739f15
Ran Crump <rancrump18@gmail.com>
2020-10-18 12:46:07 -0500

    + Birthgird size cap, Enemy Bird added consumable on 100 health, Updated the size change to match with full playable, added consumable amt to consume a consumable :100:

--- ShantyJam5/Assets/BirdGirth.cs
+++ ShantyJam5/Assets/BirdGirth.cs
@@ -8,19 +8,11 @@ public class BirdGirth : MonoBehaviour
     public int currentHealth = 25;
     public bool gameState_Lose = false, gameState_win = false;    
     public HealthBar healthBar;    
-    public float girthAddSize = 1.075f, scale = 1;
+    public float girthAddSize = .1f, scale = 1;
 
-    // Start is called before the first frame update
     void Start()
     {
         healthBar.SetHealth(currentHealth);
-        Consumable.ThingConsumed += ConsumableConsumed;
-    }
-
-    void ConsumableConsumed(Consumable c) 
-    {
-        this.scale *= girthAddSize;
-        this.transform.localScale = new Vector3(this.scale, this.scale, this.scale);
     }
 
     void OnEnable() 
@@ -35,13 +27,31 @@ public class BirdGirth : MonoBehaviour
     void OnThingConsumed(Consumable consumable)
     {
         AddGirth(Mathf.RoundToInt(consumable.GrithValue));
-    }    
+        AddSize();
+    }   
+
+    void AddSize()
+    {
+        this.scale += girthAddSize;
+        UpdateSize();
+    }
+    void SubtractSize()
+    {
+        this.scale -= girthAddSize;
+        UpdateSize();
+    }
+    void UpdateSize()
+    {        
+        scale = Mathf.Clamp(this.scale, .5f, 4);
+        this.transform.localScale = new Vector3(this.scale, this.scale, this.scale);
+    }
 
     void ChangeHealth(int change) => currentHealth = Mathf.Clamp(currentHealth + change, minGirth, maxGirth);
     void TakeDamage(int damage)
     {
         ChangeHealth(-damage);
         healthBar.SetHealth(currentHealth);
+        SubtractSize();
     }
 
     void AddGirth(int gain)
